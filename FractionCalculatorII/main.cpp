493022539906a4df4d4b3bf6830a4247f014a6c6
ryan2135 <ryan2135@pacificu.edu>
2022-06-20 20:45:58 -0700

    Add project files.

---
+++ FractionCalculatorII/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+File name:   main.cpp
+Author:      Doug Ryan
+Date:
+Class:       Intro to C++
+Assignment:  Simple Fraction Calculator
+Purpose:     To implement a simple integer calculator that works as
+						 follows.
+
+						 *** Fraction Calculator ***
+
+						 Enter: 1/2 + 2/3
+						 Answer: 7/6
+
+						 *** Fraction Calculator ***
+
+						 Enter: 0/0
+*****************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
